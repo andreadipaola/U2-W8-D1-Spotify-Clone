a7c9ade95e130c8735b86df5b51531b13c3184f1
phyxius23 <30ntin@gmail.com>
2023-03-23 17:17:59 +0100

    update style & page

--- assets/js/album.js
+++ assets/js/album.js
@@ -4,79 +4,60 @@ const id = new URLSearchParams(window.location.search).get("id");
 //const id ="130472112";
 // const id ="130472112";
 
-
-
 const fetchAlbum = async () => {
-    try {
-      const res = await fetch(URL + id);
-      const album = await res.json();
+  try {
+    const res = await fetch(URL + id);
+    const album = await res.json();
+
+    const { title, cover_medium, nb_tracks, duration, artist, tracks } = album;
+    let time = duration;
+    const hours = Math.floor(time / 3600);
+    time = time - hours * 3600;
+    const minutes = Math.floor(time / 60);
   
-      const { title, cover_medium, nb_tracks, duration, artist, tracks } = album;
-      let time = duration;
-      const hours = Math.floor(time / 3600);
-      time = time - hours * 3600;
-      const minutes = Math.floor(time / 60);
-    //   const seconds = time - minutes * 60;
-   
-      const finalTime = hours + ' ore ' + minutes + " minuti";
-      console.log(time);
-      console.log(finalTime);
-   
+    const finalTime = hours + ' ore ' + minutes + " minuti";
   
-      document.getElementById("album-cover").src = cover_medium;
-      document.getElementById("foto-gruppo").src = artist.picture_small;
-      document.getElementById("link-artist").href = `./artist.html?id=${artist.id}`;
-        console.log(album);
-      document.getElementById("album-title").innerText = title;
-      document.getElementById("autore").innerText = artist.name;
-      document.getElementById("n-brani").innerText = nb_tracks + " brani";
-      document.getElementById("durata").innerText = finalTime;
+    document.getElementById("album-cover").src = cover_medium;
+    document.getElementById("foto-gruppo").src = artist.picture_small;
+    document.getElementById("link-artist").href = `./artist.html?id=${artist.id}`;
+    document.getElementById("album-title").innerText = title;
+    document.getElementById("autore").innerText = artist.name;
+    document.getElementById("n-brani").innerText = nb_tracks + " brani";
+    document.getElementById("durata").innerText = finalTime;
 
+    const tracksContainer = document.getElementById("tracks-container");
+    const newTracks = tracks.data;
+    console.log(newTracks);
 
+    newTracks.forEach((track, index) => {
+      const trackTitle = track.title;
+      const trackRank = track.rank;
+      const trackDuration = track.duration;
+      const trackArtist = track.artist.name;
 
-
-      const tracksContainer = document.getElementById("tracks-container");
-      const newTracks = tracks.data;
-      console.log(newTracks);
-      newTracks.forEach((track, index) => {
-
-        const trackTitle = track.title;
-        console.log(trackTitle);
-        const trackRank = track.rank;
-        console.log(trackRank);
-        const trackDuration = track.duration;
-        console.log(trackDuration);
-        const trackArtist = track.artist.name;
-        console.log(trackArtist);
-
-        const min = Math.floor(trackDuration / 60);
-        const sec = trackDuration - min * 60;
-        const finalDuration = min + ':' + str_pad_left(sec, '0', 2);
+      const min = Math.floor(trackDuration / 60);
+      const sec = trackDuration - min * 60;
+      const finalDuration = min + ':' + str_pad_left(sec, '0', 2);
 
       const canzone = document.createElement('div');
       canzone.classList.add("mb-3");
       canzone.innerHTML= `
-      <div class="canzone d-flex align-items-center flex-nowrap mx-0">
-        <div class="px-4">
-            <div class="n">${index + 1}</div>
-        </div>
-        <div class=" text-nowrap flex-grow-1">
-            <!--<div class="row flex-column">
-                <div class="col song-title text-nowrap">${trackTitle}</div>
-                <div class="col autore">${trackArtist}</div>
-            </div>--!>
-            <p class="mb-1">${trackTitle}</p>
-            <p class="mb-0">${trackArtist}</p>
-        </div>
-        <div class=" px-4 text-end d-none d-md-block">${trackRank}</div>
-        <div class=" px-4 text-end d-md-none"><i class="bi bi-three-dots-vertical"></i></div>
-        <div class=" px-4 text-end d-none d-md-block">${finalDuration}</div>
+        <div class="canzone d-flex align-items-center flex-nowrap mx-0">
+          <div class="px-4">
+              <div class="n">${index + 1}</div>
+          </div>
+          <div class=" text-nowrap flex-grow-1">
+              <p class="mb-1">${trackTitle}</p>
+              <a href="./artist.html?id=${artist.id}"><p class="mb-0">${trackArtist}</p></a>
+          </div>
+          <div class=" px-4 text-end d-none d-md-block">${trackRank}</div>
+          <div class=" px-4 text-end d-md-none"><i class="bi bi-three-dots-vertical"></i></div>
+          <div class=" px-4 text-end d-none d-md-block">${finalDuration}</div>
         </div>`
-  tracksContainer.appendChild(canzone);
-});
+      tracksContainer.appendChild(canzone);
+    });
 
-  
-    //   fetchTrackList(tracklist)
+
     }
     catch (err) {
       console.log(err);
